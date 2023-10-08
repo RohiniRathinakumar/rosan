43655f403c86e730bb1966d6d9a56ac8759eeb4d
Your Name <you@example.com>
2023-10-08 08:12:06 -0500

    hi

---
+++ src/test/java/various/Signin.java
@@ -0,0 +1,37 @@
+package various;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Signin {
+	public static void main(String[] args) {
+		/*System.setProperty("webdriver.chrome.driver", "C:\\Users\\rohik\\NewEclipseSelenium\\user\\data1\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.techfios.com/");
+		driver.findElement(By.xpath("//b[text()='Login']")).click();
+		
+		List<WebElement>myList=(List<WebElement>) driver.findElement(By.xpath("//b[text()='Login']"));
+		System.out.println("Size"+myList.size());
+		for(WebElement x: myList) {
+			System.out.println("tagName"+x.getTagName());
+			System.out.println("Attribute value"+x.getAttribute("class"));*/
+		printme();
+		printmy();
+		}
+
+public static void printme() {
+		System.out.println("First git");
+		
+	}
+public static void printmy() {
+	System.out.println("2nd git");
+	
+}
+	
+	}
+
+
